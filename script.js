3e0b57c76ef3f09cad02b64e245789a89667ddeb
mosweta-school <deograciousmoriasi10@gmail.com>
2026-04-21 16:43:12 +0300

    changed the name of a function which made the system to fail

--- script.js
+++ script.js
@@ -692,7 +692,7 @@ function currentCustomerWashesHTML(currentWashes) {
         </div>`
                         }
 
-function cutomerWashesHTML(myWashes){
+function customerWashesHTML(myWashes){
                             return `
                             <div class="bg-white p-6 rounded-xl shadow mt-6">
     <h3 class="text-lg font-semibold mb-4">My washes</h3>
